63997bae115bc4835148e2f0b12a72dfcea3d2a6
panduagam <pandupanatagama123@gmail.com>
2024-10-28 10:57:34 +0700

    jadwal provider, jadwal controller, dialog ui, formpencarian, halaman utama ui

--- src/main/java/com/controllers/JadwalController.java
+++ src/main/java/com/controllers/JadwalController.java
@@ -1,5 +1,26 @@
 package src.main.java.com.controllers;
 
+import src.main.java.com.providers.JadwalProvider;
+import src.main.java.com.models.Jadwal;
+import java.util.List;
+
 public class JadwalController {
-    int TEST;
+    private JadwalProvider jadwalProvider;
+
+    public JadwalController() {
+        this.jadwalProvider = new JadwalProvider();
+    }
+
+    public List<Jadwal> cariJadwal(String lokasi) {
+        try {
+            List<Jadwal> jadwalList = jadwalProvider.getJadwalByLokasi(lokasi);
+            if (jadwalList.isEmpty()) {
+                System.out.println("Lokasi tidak terdeteksi");
+            }
+            return jadwalList;
+        } catch (Exception e) {
+            System.out.println("Koneksi Database Error: " + e.getMessage());
+            return null;
+        }
+    }
 }
--- src/main/java/com/ui/DialogUI.java
+++ src/main/java/com/ui/DialogUI.java
@@ -1,5 +1,9 @@
 package src.main.java.com.ui;
 
+import javax.swing.JOptionPane;
+
 public class DialogUI {
-    
+    public void show(String message) {
+        JOptionPane.showMessageDialog(null, message);
+    }
 }
