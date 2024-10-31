5f9434de3de72ada93b1d8caf00b2a35470a1d36
reishash <athaya27ramdhany@gmail.com>
2024-10-31 18:03:26 +0700

    main

--- src/main/java/com/Main.java
+++ src/main/java/com/Main.java
@@ -1,5 +1,19 @@
 package src.main.java.com;
+
+import src.main.java.com.controllers.JadwalController;
+import src.main.java.com.controllers.LoginController;
+import src.main.java.com.controllers.LokasiController;
+import src.main.java.com.ui.HalamanAwalUI;
+
 public class Main {
     public static void main(String[] args) {
+        // Inisialisasi controller yang diperlukan
+        LoginController loginController = new LoginController();
+        JadwalController jadwalController = new JadwalController();
+        LokasiController lokasiController = new LokasiController();
+
+        // Menampilkan halaman awal aplikasi
+        HalamanAwalUI halamanAwalUI = new HalamanAwalUI(loginController, jadwalController, lokasiController);
+        halamanAwalUI.tampilkan();
     }
 }
